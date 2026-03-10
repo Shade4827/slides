f388eba09860663acba9665994bd360e671909ed
Shade4827 <shade4827@gmail.com>
2026-03-10 20:57:05 +0900

    ヘッダー・フッターを追加し、基本のスタイルを設定

---
+++ nuxt/app/components/TheFooter.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer>
+    <p class="text-center text-sm text-gray-400">© 2026 Shade4827. All rights reserved.</p>
+  </footer>
+</template>
---
+++ nuxt/app/components/TheHeader.vue
@@ -0,0 +1,5 @@
+<template>
+  <header>
+    <h1 class="mb-4 font-bold text-xl border-b-2 border-white">slides.shade4827.net</h1>
+  </header>
+</template>
