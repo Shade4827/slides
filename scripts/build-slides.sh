dc4d6adb3508ac04443912837c20c3a0fe59c6e6
Shade4827 <shade4827@gmail.com>
2026-03-18 21:20:05 +0900

    ディレクトリ内の全てのスライドに対してビルドをするようにスクリプトを修正

--- scripts/build-slides.sh
+++ scripts/build-slides.sh
@@ -2,10 +2,20 @@
 
 set -e
 
-SLIDES_DIR="slides/slides"
-OUTPUT_DIR="nuxt/public/slides"
+ROOT_DIR=$(git rev-parse --show-toplevel)
 
-pnpm slidev build "$SLIDES_DIR/sample/slides.md" \
-  --base /slides/sample/ \
-  --out ../../../nuxt/public/slides/sample \
-  --without-notes
+SLIDES_DIR="$ROOT_DIR/slides/slides"
+OUTPUT_DIR="$ROOT_DIR/nuxt/public/slides"
+
+for dir in $SLIDES_DIR/*; do
+  if [ -d "$dir" ]; then
+    name=$(basename "$dir")
+
+    echo "Building slide: $name"
+
+    pnpm slidev build "$dir/slides.md" \
+      --base "/slides/$name/" \
+      --out "$OUTPUT_DIR/$name" \
+      --without-notes
+  fi
+done
