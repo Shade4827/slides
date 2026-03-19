d86cde659d792128f18b8faae33ac8e19aaa5a5d
Shade4827 <shade4827@gmail.com>
2026-03-19 16:43:10 +0900

    存在しないスライドにアクセスした時、エラーページを表示する

--- nuxt/app/pages/slide-view/[slideId]/index.vue
+++ nuxt/app/pages/slide-view/[slideId]/index.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
+import type { SlideCard } from '~/types/SlideCard'
 const route = useRoute()
 
 const slideUrl = computed(() => {
   return `/slides/${route.params.slideId}/`
 })
+
+const { data: slides } = await useFetch<SlideCard[]>(`${useRequestURL().origin}/slides.json`)
+
+const slide = slides.value?.find(slide => slide.link === route.params.slideId)
+
+if (!slide) {
+  throw createError({ statusCode: 404 })
+}
 </script>
 
 <template>
