fd5ab664ca849282ce4e5c9c968c3eaa354845a1
Shade4827 <shade4827@gmail.com>
2026-03-21 22:33:42 +0900

    一覧ページ内のスタイルを調整

--- nuxt/app/components/SlideCard.vue
+++ nuxt/app/components/SlideCard.vue
@@ -9,9 +9,9 @@ const { slideCard } = defineProps<Props>()
 </script>
 
 <template>
-  <div>
-    <a :href="`/slide-view/${slideCard.link}`" class="block">
-      <div class="w-full h-48 bg-gray-200 rounded-lg overflow-hidden">
+  <div class="transition hover:opacity-50 mb-2">
+    <NuxtLink :href="`/slide-view/${slideCard.link}`" class="block">
+      <div class="w-full h-48 bg-gray-200 rounded-lg overflow-hidden mb-2">
         <img
           v-if="slideCard.thumbnail"
           :src="slideCard.thumbnail"
@@ -19,7 +19,7 @@ const { slideCard } = defineProps<Props>()
           class="w-full h-full object-cover"
         >
       </div>
-      <h2 class="text-xl font-semibold mt-4">{{ slideCard.title }}</h2>
-    </a>
+      <h2 class="text-xl font-semibold">{{ slideCard.title }}</h2>
+    </NuxtLink>
   </div>
 </template>
